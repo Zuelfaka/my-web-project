dcf57d0ddbb64d24450654c2d660def1c39da3b4
monicell <m17222564@s.uokerbala.edu.iq>
2025-04-07 21:53:11 +0300

    تحسين إعادة التوجيه بعد تسجيل الخروج في index.php

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php
 session_start();
 
-// If the user is logged in, redirect them to the dashboard
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
     exit();
 } else {
-    // If the user is not logged in, check if there's a logout success message in the URL
     if (isset($_GET['logout']) && $_GET['logout'] == 'success') {
-        // Show a logout success message
         $logout_message = "تم تسجيل الخروج بنجاح";
+        header("Location: login.php?logout_message=" . urlencode($logout_message));
+        exit();
     } else {
-        $logout_message = null;
+        header("Location: login.php");
+        exit();
     }
-
-    // Redirect the user to the login page
-    header("Location: login.php?logout_message=" . urlencode($logout_message));
-    exit();
 }
 ?>
